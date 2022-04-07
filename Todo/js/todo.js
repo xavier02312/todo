f532472bc5842a33899f3b66d1570d20258a9b22
xavier02312 <paqueriaudxavier@gmail.com>
2022-04-07 16:51:13 +0200

    ex

---
+++ Todo/js/todo.js
@@ -0,0 +1,45 @@
+/**
+ * TODO LIST
+ */
+
+/**
+ * Notes :
+ * ---
+ * Un élément ajouté à la liste devra systématiquement posséder ce code HTML :
+ * <li class="list-group-item d-flex justify-content-between">
+ *     An item
+ *     <a href="#" id="deleteItem">
+ *         <img src="imgs/trash.svg" alt="Delete item" class="icon-delete">
+ *     </a>
+ * </li>
+ * 
+ * 
+ *//*Etape #1*/
+//*Ecouter l' envoi du formulaire
+//*Récupérer la valeur du champs
+//*Insérer cette valeur dans le tableau "todos"
+//*Bonus : Remettre le champs du formulaire à "zéro"
+/**
+ * Etape #2
+ * Crée un fichier "fonction.js"
+ * crée une fonction permettant d'afficher les éléments du
+ * tableau dans la "ul" du fichier HTML en respectant le code
+ * HTML fourni plus haut
+ */
+/**Etape #3
+ * Appeler la fonction précédente dans l'évènement du formulaire
+ * après
+ */
+// Contiendra les éléments de notre liste à cocher
+let todos = [];
+
+todos.forEach(item)
+
+
+const iconDelete = document.querySelectorAll(".icon-delete");
+
+iconDelete.addEvenListeber("click", () => {
+
+      
+});
+
